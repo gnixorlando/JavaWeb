1b7781fe930e3ef875bba39cf0af3d4f751d2c5f
gnixorlando <gabyorlandonascimento@gmail.com>
2023-01-18 20:51:04 -0300

    create delete-select-update methods

--- src/main/java/br/com/exemplo/jdbc/UsuarioDAO.java
+++ src/main/java/br/com/exemplo/jdbc/UsuarioDAO.java
@@ -2,32 +2,93 @@ package br.com.exemplo.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.exemplo.beans.Usuario;
 
 public class UsuarioDAO {
 	private Connection con = Conexao.getConnection();
-	
-	public void cadastro (Usuario usuario) {
-		
-		String sql = "INSERT INTO USUARIO (nome, email, senha) values (?, ?, ?)";
-			PreparedStatement prepared;
-			try {
-				prepared = con.prepareStatement(sql);
-				prepared.setString(1, usuario.getNome());
-				prepared.setString(2, usuario.getEmail());
-				prepared.setString(3, usuario.getSenha());
-				
-				prepared.execute();
-				prepared.close();
 
-				System.out.println("Usuário cadastrado com sucesso!");
-				
-			} catch (SQLException e) {
-				
-				System.out.println(e);
-			}
+	public void cadastro(Usuario usuario) {
+
+		String sql = "INSERT INTO USUARIO (nome, email, senha) values (?, ?, ?)";
+		PreparedStatement prepared;
+		try {
+			prepared = con.prepareStatement(sql);
+			prepared.setString(1, usuario.getNome());
+			prepared.setString(2, usuario.getEmail());
+			prepared.setString(3, usuario.getSenha());
+
+			prepared.execute();
+			prepared.close();
+
+			System.out.println("Usuário cadastrado com sucesso!");
+
+		} catch (SQLException e) {
+
+			System.out.println(e);
+		}
+
+	}
+
+	public void edit(Usuario usuario) {
+		String sql = "UPDATE USUARIO SET NOME = ?, EMAIL = ?,SENHA = ? where id = ?";
+		PreparedStatement prepared;
+		try {
+			prepared = con.prepareStatement(sql);
+			prepared.setString(1, usuario.getNome());
+			prepared.setString(2, usuario.getEmail());
+			prepared.setString(3, usuario.getSenha());
+			prepared.setInt(4, usuario.getId());
+
+			prepared.execute();
+			prepared.close();
+
+			System.out.println("Usuário alterado com sucesso!");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public void remove(Usuario usuario) {
+		String sql = "DELETE FROM USUARIO WHERE id = ?";
+		PreparedStatement prepared;
+
+		try {
+			prepared = con.prepareStatement(sql);
+			prepared.setInt(1, usuario.getId());
 			
+			prepared.execute();
+			prepared.close();
+
+			System.out.println("Usuário removido com sucesso!");
+		} catch (Exception e) {
+
+			System.out.println(e);
+		}
+		}
+
+	public List<Usuario> searchAll (Usuario usuario) {
+		String sql = "SELECT * FROM USUARIO";
+		List<Usuario> list = new ArrayList<Usuario>(); 
+		try {
+			PreparedStatement prepared =con.prepareStatement(sql);
+			ResultSet result = prepared.executeQuery();
+			while (result.next()) {
+				Usuario usu = new Usuario();
+				usu.setId(result.getInt("id"));
+				usu.setNome(result.getString("nome"));
+				usu.setEmail(result.getString("email"));
+				usu.setSenha(result.getString("senha"));
+				list.add(usu);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		} return list;
 	}
 }
--- src/main/java/br/com/exemplo/testes/TestDAO.java
+++ src/main/java/br/com/exemplo/testes/TestDAO.java
@@ -1,11 +1,13 @@
 package br.com.exemplo.testes;
 
+import java.util.List;
+
 import br.com.exemplo.beans.Usuario;
 import br.com.exemplo.jdbc.UsuarioDAO;
 
 public class TestDAO {
 		public static void main(String[] args) {
-			testCadastro();
+			testSearchAll();
 		}
 		public static void testCadastro () {
 			Usuario usuario = new Usuario ();
@@ -17,5 +19,46 @@ public class TestDAO {
 			UsuarioDAO usuDao = new UsuarioDAO();
 			usuDao.cadastro(usuario);
 			
+		}
+		public static void testEdit () {
+			
+			Usuario usuario = new Usuario ();
+			
+			usuario.setId(3);
+			usuario.setNome("João Vitor Targino");
+			usuario.setEmail("joaovitortargino12@gmail.com");
+			usuario.setSenha("gerencieaqui");
+			
+			
+			UsuarioDAO usuDao = new UsuarioDAO();
+			usuDao.edit(usuario);
+		}
+		
+		public static void testRemove () {
+			Usuario usuario = new Usuario ();
+			
+			usuario.setId(4);
+			
+			UsuarioDAO usuDao = new UsuarioDAO();
+			usuDao.remove(usuario);
+		}
+		
+		public static void mostrarInformacoes () {
+			Usuario usuario = new Usuario ();
+			System.out.println (usuario.getId() + usuario.getNome()+usuario.getEmail()+ usuario.getSenha() );
+			
+			
+		}
+		public static void testSearchAll () {
+			
+			Usuario usuario = new Usuario ();
+			UsuarioDAO usuDao = new UsuarioDAO();
+			
+			List<Usuario> listResult = usuDao.searchAll(usuario);
+			
+			for (Usuario u:listResult) {
+				
+				System.out.println(" Id: " + u.getId() + " Nome: " + u.getNome() + " Email: " + u.getEmail() + " Senha: " + u.getSenha());
+			}
 		}
 }
